9d36621ab8fe356abe71f6c5c7ab41519ac3eb32
Gabriel Rodrigues <gabrielragonhaprofissional@outlook.com>
2025-02-25 17:04:49 -0300

    feat: maximize window

--- src/electron/main.ts
+++ src/electron/main.ts
@@ -1,9 +1,18 @@
 import { app, BrowserWindow } from "electron";
 import path from "path";
+import getPreloadPath from "./pathResolver.js";
 import { isDev } from "./util.js";
 
 app.on("ready", () => {
-  const mainWindow = new BrowserWindow({});
+  const mainWindow = new BrowserWindow({
+    webPreferences: {
+      preload: getPreloadPath(),
+      contextIsolation: true,
+    },
+    show: false,
+  });
+
+  mainWindow.maximize();
 
   if (isDev()) {
     mainWindow.loadURL("http://localhost:5123/");
